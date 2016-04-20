4001ebce72a2f374c6754a4e8c1cac9c8fceff03
iganbold <itgel.ganbold1@gmail.com>
2016-04-20 16:32:39 +0000

    Add printMenu()

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+void printMenu();
+
 int main() {
-    
+    printMenu();
+}   
+
+void printMenu() {
+    printf("(1) Display current date and time\n");
+    printf("(2) Display the calendar of the current month\n");
+    printf("(3) List the files in the current directory\n");
+    printf("(4) Exit from the program\n");
 }
